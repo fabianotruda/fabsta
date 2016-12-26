4014eae2d33ebc98e0e7c166d1a89778c41953f0
Gianluca Truda <gianlucatruda@gmail.com>
2016-12-26 15:26:08 +0200

    Added enhanced animations.

--- js/animations.js
+++ js/animations.js
@@ -1,10 +1,33 @@
 (function(){
 
   const header1 = document.getElementsByClassName("fabsta");
+  const posts = document.getElementsByClassName("post");
+  const navItem = document.getElementsByClassName("navItem");
 
-  TweenMax.from(header1, 0.8, 
-    { x: 200, autoAlpha: 0, ease: Power4.easeInOut }
+  const tl1 = new TimelineMax();
+
+  tl1.add(
+  	TweenMax.from(header1, 1.5, 
+    	{ y: -100, autoAlpha: 0, ease: Power2.easeInOut }
+  	),
+  	'0.3'
   );
+
+  tl1.add(
+  	TweenMax.staggerFrom(navItem, 0.5, 
+    	{ y: 10, autoAlpha: 0, ease: Power4.easeInOut }, 0.2
+  	),
+  	'+=0'
+  );
+
+  tl1.add(
+  	TweenMax.from(posts, 2, 
+    	{ y: -50, autoAlpha: 0, ease: Power2.easeInOut }
+  	),
+  	'+=1'
+  );
+
+
 
 
 }());
