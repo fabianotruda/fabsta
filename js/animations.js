59d4da0534fd0f7a86dc697d233e7708bb2e555d
Gianluca Truda <gianlucatruda@gmail.com>
2016-12-26 13:56:50 +0200

    Fixed html formatting. Removed redundant files. Fixed css

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,6 @@
 (function(){
 
-  const header1 = document.getElementsByClassName("h1");
+  const header1 = document.getElementsByClassName("fabsta");
 
   TweenMax.from(header1, 1.5, 
     { x: 200, autoAlpha: 0, ease: Power3.easeInOut }
